462746173dd73b0f4a33671b8b0b5abdc5a266fa
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2023-06-23 00:52:34 -0700

    remove ko, fix copyright, fix linter errors

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM alpine
-RUN apk --no-cache add ca-certificates && apk --no-cache add tzdata
-WORKDIR /app/
-COPY . .
-CMD ["/app/bin/nnhntr"]
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"embed"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -16,11 +17,16 @@ var (
 	//go:embed styles
 	styles embed.FS
 
-	//go:embed index.html
-	indexHTML []byte
+	//go:embed index.html.template
+	indexHTML string
 )
 
+type PageData struct {
+	Year int
+}
+
 func main() {
+	templates := template.Must(template.New("").Parse(indexHTML))
 	start := time.Now()
 	mux := http.NewServeMux()
 	mux.Handle("/images/", http.FileServer(http.FS(images)))
@@ -34,7 +40,14 @@ func main() {
 			http.Error(res, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 			return
 		}
-		http.ServeContent(res, req, "index.html", start, bytes.NewReader(indexHTML))
+		var buf bytes.Buffer
+		if err := templates.Execute(&buf, PageData{
+			Year: time.Now().Year(),
+		}); err != nil {
+			http.Error(res, "failed to render page", http.StatusInternalServerError)
+			return
+		}
+		http.ServeContent(res, req, "index.html", start, bytes.NewReader(buf.Bytes()))
 	})
 	log.Println("starting server...")
 	log.Fatalln(http.ListenAndServe(":"+os.Getenv("PORT"), mux))
